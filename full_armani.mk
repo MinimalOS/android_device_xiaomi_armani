82cbfa0aaeabbcfaecb4e0e03ee72ee1cbb1b742
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2015-07-03 19:24:06 +0200

    Minimal-ify
    Signed-off-by: Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>

--- full_armani.mk
+++ full_armani.mk
@@ -21,17 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/armani/device.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := full_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=armani
